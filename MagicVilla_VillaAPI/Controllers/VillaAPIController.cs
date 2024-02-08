2a5e60915a994b5a82132ad8dc60a2bdc18df49d
Georgios Skouras <g.skouras@kaizengaming.com>
2024-02-08 15:59:08 +0200

    feat: implement Villa CRUID operations

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,49 @@
+﻿using MagicVilla_VillaAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/villa")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    { 
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GeVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}", Name="GetVilla")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villa)
+        {
+            if (villa == null)
+            {
+                return BadRequest(villa);
+            }
+
+            villa.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villa);
+
+            return CreatedAtRoute("GetVilla", new { id = villa.Id},villa);
+        }
+    }
+}
